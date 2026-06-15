2732da814478417aff4b1eed1418a45e9cf61772
Olof Fredriksson <oloflolof@gmail.com>
2026-06-15 10:25:24 +0000

    fix: create hooks without external dependency

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,6 +1,9 @@
+/* 
+    Both used internally by this repo but also used by the consumer.
+*/
 import { fileURLToPath } from "node:url";
 
-const lintConfigPath = fileURLToPath(import.meta.resolve("./lint-staged.js"));
+const lintConfigPath = fileURLToPath(import.meta.resolve("../lint-staged.js"));
 
 export default {
     "pre-commit": `npm exec lint-staged -- --config "${lintConfigPath}" "$@"`,
