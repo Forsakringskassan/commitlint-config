483e83e5cc643a22a3557714c11084a7e5810251
Olof Fredriksson <oloflolof@gmail.com>
2026-06-16 16:47:41 +0000

    feat: remove postinstall hook
    BREAKING CHANGE: The postinstall hook has been removed, and the package no longer automatically installs git hooks.
    Users must now manually add `commitlint-config` into their prepare section in package.json
    
    ```bash
    $ npm pkg set scripts.prepare="commitlint-config"
    ```
    
    Changes is due to updates in upcoming version of npm 12 which changes the behavior of postinstall scripts.
    More info regarding NPM 12 changes can be found here:
    * https://github.blog/changelog/2026-06-09-upcoming-breaking-changes-for-npm-v12/

--- vitest.global.js
+++ vitest.global.js
@@ -4,8 +4,6 @@ import { execSync } from "node:child_process";
 const rootDir = import.meta.dirname;
 
 export async function setup() {
-    // eslint-disable-next-line camelcase -- npm setting
-    const env = { ...process.env, npm_config_ignore_scripts: undefined };
-    execSync("npm run build", { cwd: rootDir, stdio: "inherit", env });
-    execSync("npm pack", { cwd: rootDir, stdio: "inherit", env });
+    execSync("npm run build", { cwd: rootDir, stdio: "inherit" });
+    execSync("npm pack", { cwd: rootDir, stdio: "inherit" });
 }
